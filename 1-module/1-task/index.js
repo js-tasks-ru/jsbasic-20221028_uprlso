6a879f4dba06817bd832fbc17ffecfd6bc04f98e
sershva <uprlso@gmail.com>
2022-10-31 20:39:40 +0700

    Функция для получения факториала с проверками, циклом forи работой с массивом

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,31 @@
-function factorial(n) {
-  // ваш код...
+function factorial(number) {
+  if (isNaN(number) ||
+      number < 0 ||
+      number === ""
+  ) {
+      return null;
+  }
+  
+  if (number === 0 || number === 1) {
+      return 1;
+  }
+
+  if (number > 170) {
+      return Infinity
+  }
+
+  let factorialChain = [];
+  let result;
+
+  for(let j = 0; j <= number; j++){
+      if (j > 0) {
+          factorialChain.push(j);
+      }
+  }
+
+  result = factorialChain.reduce((multiplication, element) =>{
+      return multiplication * element;
+  })
+
+  return result
 }
