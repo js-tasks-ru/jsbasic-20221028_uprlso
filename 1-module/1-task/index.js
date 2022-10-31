567eeedcac40960e0644cc89ea164618e7f9b3f7
sershva <uprlso@gmail.com>
2022-10-31 21:08:29 +0700

    Функция для вычисления факториала с использованием: проверок, цикла for и методов работы с массивом

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -3,29 +3,27 @@ function factorial(number) {
       number < 0 ||
       number === ""
   ) {
-      return null;
+    return null;
   }
   
   if (number === 0 || number === 1) {
-      return 1;
+    return 1;
   }
   // после 170 функция выдает бесконечность
   if (number > 170) {
-      return Infinity
+    return Infinity
   }
 
   let factorialChain = [];
   let result;
 
-  for(let j = 0; j <= number; j++){
-      if (j > 0) {
-          factorialChain.push(j);
-      }
+  for(let j = 1; j <= number; j++){
+    factorialChain.push(j);
   }
 
-  result = factorialChain.reduce((multiplication, element) =>{
-      return multiplication * element;
+  result = factorialChain.reduce((multiplication, element) => {
+    return multiplication * element;
   })
 
-  return result
+  return result;
 }
