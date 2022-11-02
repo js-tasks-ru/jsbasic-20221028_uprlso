babd5ef78f3e1a7ba350a8455f3c3e3509c8a30f
sershva <uprlso@gmail.com>
2022-11-02 21:14:55 +0700

    Функция для проверки на спам

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,11 @@
-function checkSpam(str) {
-  // ваш код...
-}
+const checkSpam = ((str) => {
+  let spamOne = "1xBet".toLocaleLowerCase();
+  let spamTwo = "XXX".toLocaleLowerCase();
+  let smallStr = str.toLocaleLowerCase();
+
+  if  (smallStr.includes(spamOne) || smallStr.includes(spamTwo))  {
+    return true;
+  }  else  {
+    return false;
+  }
+})
