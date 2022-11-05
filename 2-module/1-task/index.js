acb4ce3dfa56727d0a71b07f186f6cd9d1fb44c7
sershva <uprlso@gmail.com>
2022-11-05 20:45:16 +0700

    Функция для суммирования зарплат

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,29 @@
-function sumSalary(salaries) {
-  // ваш код...
+function sumSalary(salaries)  {
+  let sum2 = {...salaries};
+  let sum = [];
+  let result;
+
+  for (key in sum2)  {
+    let variable = sum2[key];
+    
+    if (isNaN(variable) ||
+      variable === "" ||
+      typeof variable == "boolean" ||
+      !isFinite(variable)
+    )  {
+       continue
+    }  else  {
+       sum.push(variable);
+    }
+  }
+
+  if (sum.length === 0)  {
+    return 0;
+  }  else  {
+    result = sum.reduce((accum,element) => {
+      return accum + element;
+    })
+  }
+
+  return result;
 }
