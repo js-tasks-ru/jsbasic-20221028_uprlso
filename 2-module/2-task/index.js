89c4db3e2f9a7742db205708b7f8d09d2dd60f85
sershva <uprlso@gmail.com>
2022-11-06 17:51:05 +0700

    Функция для проверки пустого объекта

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -1,3 +1,17 @@
-function isEmpty(obj) {
-  // ваш код...
+function isEmpty(obj)  {
+  let newObj = {...obj};
+
+  if (Object.keys(newObj).length === 0)  {
+      return true;
+  }
+
+  for (key in newObj)  {
+    let element = newObj[key];
+
+    if (element === undefined || 
+        element
+    )  {
+        return false;
+    }
+  }
 }
