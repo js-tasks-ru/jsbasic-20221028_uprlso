af19cc38b09e4ac411403ca8ddf42a6ad2e181c6
sershva <uprlso@gmail.com>
2022-11-15 00:23:40 +0700

    Функция для показа зарплать

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,17 @@
 function showSalary(users, age) {
-  // ваш код...
+  let copyUser = JSON.parse(JSON.stringify(users));
+  let foundedUsers = [];
+  let result = [];
+  copyUser.filter(item =>  {
+    if (item["age"] <= age)  {
+      foundedUsers.push(item);
+    }
+  })
+  foundUsers.forEach((element) => {
+    let word = element["name"];
+    let balance = element["balance"];
+    let contain = [word, balance];
+    result.push(contain.join(", "));
+  })
+  return result.join(`\n`);
 }
