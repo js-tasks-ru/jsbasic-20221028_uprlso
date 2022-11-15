4f0076fd3af1fc4ba1b8cfe1c73695847ced799d
sershva <uprlso@gmail.com>
2022-11-15 18:42:30 +0700

    Функция min max

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,34 +1,32 @@
 function getMinMax(str) {
+  let masNum = str.split(" ");
+  let numNum = [];
 
-  let masNum = str.split(" ")
-
-  let numNum = []
-
-  let findNum = masNum.filter(item =>  {
-    if (!isNaN(item)){
-        return numNum.push(parseFloat(item))
+  let findNum = masNum.filter(item  =>  {
+    if (!isNaN(item))  {
+      return numNum.push(parseFloat(item));
     }
-  })
-  let sortnNum = selecSort(numNum)
+  });
   
-  let x = sortnNum[0]
+  let sortnNum = selecSort(numNum);
+  let x = sortnNum[0];
+  let y = sortnNum[sortnNum.length - 1];
 
-  let y = sortnNum[sortnNum.length - 1]
-
-  return {min : x, max : y}
+  return {min : x, max : y};
 }
 
 function selecSort(arr)  {
-    for (let i = 0 ; i < arr.length; i++ ){
-      let indexMin = i
-      for(let j = i+1; j < arr.length; j++)  {
-        if (arr[indexMin] > arr[j])  {
-            indexMin = j
-        }
+  for (let i = 0 ; i < arr.length; i++ )  {
+    let indexMin = i;
+
+    for(let j = i+1; j < arr.length; j++)  {
+      if (arr[indexMin] > arr[j])  {
+        indexMin = j;
       }
-      let tmp = arr[indexMin]
-      arr[indexMin] = arr[i]
-      arr[i] = tmp
-   }
-   return arr;
+    }
+    let tmp = arr[indexMin];
+    arr[indexMin] = arr[i];
+    arr[i] = tmp;
+  }
+  return arr;
 }
