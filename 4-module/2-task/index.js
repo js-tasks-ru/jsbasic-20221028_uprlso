dc0033a925d5ead75a4da92612a50288dc91a8b4
sershva <uprlso@gmail.com>
2022-11-19 00:49:29 +0700

    Функция с диагональными квадратами

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,10 @@
 function makeDiagonalRed(table) {
-  // ваш код...
+  
+ let rowLength = table.rows.length
+
+ for (let i = 0; i < rowLength; i++)  {
+    table.rows[i].cells[i].style.backgroundColor = 'red';
+  }
+
+  return table;
 }
