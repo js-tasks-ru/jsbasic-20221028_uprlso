5661adacf57f0d075d12bcc8f30627ef6d061403
sershva <uprlso@gmail.com>
2022-11-22 14:17:43 +0700

    Функция highlight

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,66 @@
+let hStatAvail = (tbody => {
+  let rowLength = tbody.rows.length;
+
+  for (let i = 0; i < rowLength; i++) {
+    let rowcheckAv = tbody.rows[i];
+    let cellCheckAv = tbody.rows[i].cells;
+
+    for (cell of cellCheckAv)  {
+      if (cell.hasAttribute("data-available")) {
+        if (cell.dataset.available === "true") {
+          rowcheckAv.classList.add("available");  
+        }  else  {
+          rowcheckAv.classList.add("unavailable");
+        }
+      }
+    }
+
+    if (rowcheckAv.classList.contains("available") === false &&
+        rowcheckAv.classList.contains("unavailable") === false
+       ) {
+        rowcheckAv.hidden = "true";
+    }
+  }
+});
+
+let hCheckSex = (tbody => {
+  let rowLenSex = tbody.rows.length;
+
+  for (let i = 0; i < rowLenSex; i++) {
+    let rowCheckSex = tbody.rows[i];
+    let cellsCheckSex = tbody.rows[i].cells;
+
+    for (cell of cellsCheckSex) {
+      if (cell.innerHTML === "m") {
+        rowCheckSex.classList.add("male");
+      } 
+      if (cell.innerHTML === "f") {
+        rowCheckSex.classList.add("female");
+      }
+    }
+  }
+});
+
+let hlineStylAge = (tbody => {
+  let rowlineLen = tbody.rows.length;
+  for (let i = 0; i < rowlineLen; i++) {
+    let rowLineStyl = tbody.rows[i];
+    let cellsLineStyl = tbody.rows[i].cells;
+
+    for (cell of cellsLineStyl) {
+      let cellInfo = cell.innerHTML
+      if (!isNaN(cellInfo) && cellInfo < 18) {
+        rowLineStyl.style = "text-decoration: line-through";
+      }
+    }
+  }
+  
+});
+
 function highlight(table) {
-  // ваш код...
+  let tbody = table.querySelector("tbody");
+
+  hStatAvail(tbody);
+  hCheckSex(tbody);
+  hlineStylAge(tbody);
 }
