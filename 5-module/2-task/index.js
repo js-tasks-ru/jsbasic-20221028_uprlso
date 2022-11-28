6b948c4e697ab9196f429e41f3dbd54c8dcc58c0
sershva <uprlso@gmail.com>
2022-11-29 02:22:31 +0700

    Функция toggle text

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,6 +1,4 @@
-function toggleText()  {
-  let button = document.querySelector("button");
-
+function toggleText() {
   class hidipButton  {
     handleEvent(event)  {
       if (event.type === "click")  {
@@ -11,20 +9,22 @@ function toggleText()  {
       }
     }
   }
-  class unhidipButton {
+  class unhidipButton  {
     handleEvent(event)  {
       if (event.type === "click")  {
         let findText = document.getElementById('text');
 
         findText.removeAttribute('hidden');
+
         button.removeEventListener("click", unhidBut);
         button.addEventListener("click", hidBut);
       }
     }
   }
 
+  let button = document.querySelector("button");
   let hidBut = new hidipButton();
   let unhidBut = new unhidipButton();
-  
-  button.addEventListener("click", hidBut);
+
+  button.addEventListener("click", hidBut)
 }
